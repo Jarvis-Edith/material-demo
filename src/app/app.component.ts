3e81a744f8e1c62da69dd890884502dcd32a38a2
JarvisEdith <jarvispeculiardata@gmail.com>
2020-12-23 12:14:09 +0530

    After Angular Material Added -FC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-demo';
+  notification = 0;
+  showSpinner = false;
+
+  loadData() {
+    this.showSpinner = true;
+    setTimeout(() => {
+      this.showSpinner = false;
+    },5000);
+  }
 }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+import { MatButtonModule} from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+const material = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule
+];
+
+@NgModule({
+  imports: [material],
+  exports: [material]
+})
+  
+export class MaterialModule { }
